900a504489f10e8cc23cf503b26af15530a5a344
eun-g-kim <eunji9803@gmail.com>
2020-06-13 00:04:21 +0900

    add user pic

--- home.js
+++ home.js
@@ -33,7 +33,7 @@ function authStateObserver(user) {
   if (user) { // User is signed in!
     // Get the signed-in user's profile pic and name.
     var profilePicUrl = getProfilePicUrl();
-    document.getElementById("userpic").src = 'url(' + addSizeToGoogleProfilePic(profilePicUrl) + ')';
+    document.getElementById("userpic").src = "\"" + addSizeToGoogleProfilePic(profilePicUrl) + "\"";
     document.getElementById("userID").innerHTML = getUserName();
     document.getElementById("userID2").innerHTML = getUserName();
 
