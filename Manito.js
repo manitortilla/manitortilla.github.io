45a5f15ac485335c185a92fe965614acbe1d5f42
eun-g-kim <eunji9803@gmail.com>
2020-06-13 16:31:42 +0900

    add firebase to Manito and DB to gift_detail

--- Manito.js
+++ Manito.js
@@ -1,26 +1,74 @@
+initFirebaseAuth();
+// Initiate firebase auth.
+function initFirebaseAuth() {
+ firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+var signOutButtonElement = document.getElementById('logoutbtn');
+function signOut() {
+ // Sign out of Firebase.
+ firebase.auth().signOut();
+}
+signOutButtonElement.addEventListener('click', signOut);
+
+function getProfilePicUrl() {
+ return firebase.auth().currentUser.photoURL;
+}
+
+// Returns the signed-in user's display name.
+function getUserName() {
+ return firebase.auth().currentUser.displayName;
+}
+
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+return !!firebase.auth().currentUser;
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+ if (user) { // User is signed in!
+   // Get the signed-in user's profile pic and name.
+   var url = getProfilePicUrl();
+
+   document.getElementById("userpic").setAttribute("src", url);
+   document.getElementById("userpic2").setAttribute("src", url);
+   document.getElementById("userpic3").setAttribute("src", url);
+   document.getElementById("userpic4").setAttribute("src", url);
+   document.getElementById("userID").innerHTML = getUserName();
+   document.getElementById("userID2").innerHTML = getUserName();
+   document.getElementById("userID3").innerHTML = getUserName();
+   document.getElementById("userID4").innerHTML = getUserName();
+   document.getElementById("userID5").innerHTML = getUserName();
+
+ } else { // User is signed out!
+   location.href="/index.html";
+ }
+}
 
 // sidebar menu toggle
 $('.sidebarbtn').on('click', function(){
     //sidebarbtn color update
     $('.sidebarbtn').removeClass('on');
     $(this).addClass('on');
-  
+
     //show the related content
     var idx = $('.sidebarbtn').index(this);
     $('.detail').hide(); //style="display:none"
     $('.detail').eq(idx).show();
-    if (idx == 0){ 
+    if (idx == 0){
       $('.manitomain').show();
       $('.guesswindow').hide();
     }
-  }); 
+  });
 
 $('#guessingbutton').on('click', function(){
     if($(this).hasClass('guessbtn posi')){
         document.getElementsByClassName("manitomain")[0].style.display = "none";
         document.getElementsByClassName("guesswindow")[0].style.display = "block";
     }
-    
+
 });
 
 $('#guesswindowclose').on('click', function(){
@@ -43,7 +91,7 @@ $('#guessuserbutton1').on('click',function(){
     else{
 
     }
-    
+
 });
 
 $('#guessuserbutton2').on('click',function(){
@@ -60,7 +108,7 @@ $('#guessuserbutton2').on('click',function(){
     else{
 
     }
-    
+
 });
 $('#guessuserbutton3').on('click',function(){
     const result = confirm('Would you choose this one as Manito?');
@@ -76,5 +124,5 @@ $('#guessuserbutton3').on('click',function(){
     else{
 
     }
-    
+
 });
