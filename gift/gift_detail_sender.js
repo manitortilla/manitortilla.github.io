b8b0cd7dd63feecad24ebdfbbbc2e27823258ddc
eun-g-kim <eunji9803@gmail.com>
2020-06-16 02:33:18 +0900

    hashtag->detail 구현

--- gift/gift_detail_sender.js
+++ gift/gift_detail_sender.js
@@ -1,3 +1,38 @@
+initFirebaseAuth();
+// Initiate firebase auth.
+function initFirebaseAuth() {
+ firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+var signOutButtonElement = document.getElementById('logoutbtn');
+function signOut() {
+ // Sign out of Firebase.
+ firebase.auth().signOut();
+}
+signOutButtonElement.addEventListener('click', signOut);
+
+// Returns the signed-in user's display name.
+function getUserName() {
+ return firebase.auth().currentUser.displayName;
+}
+
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+return !!firebase.auth().currentUser;
+}
+
+// Triggers when the auth state change for instance when the user signs-in or signs-out.
+function authStateObserver(user) {
+ if (user) { // User is signed in!
+   document.getElementById("userID").innerHTML = getUserName();
+
+ } else { // User is signed out!
+   location.href="/index.html";
+ }
+}
+
+
 $('.product_buybtn').on('click', function(){
     alert("The present is sent successfully!");
 });
@@ -5,3 +40,23 @@ $('.product_buybtn').on('click', function(){
 $('#close').on('click',function(){
     $('#pop').hide();
 });
+
+
+function getProduct(Product_no){
+  var name1 = document.getElementsByClassName("product_name")[0];
+  var name2 = document.getElementsByClassName("gift_value")[2];
+  var img = document.getElementsByClassName("product_img")[0];
+  var price = document.getElementsByClassName("product_price")[0];
+  var detail = document.getElementsByClassName("product_detail")[0];
+
+  return firebase.database().ref(Product_no).once('value').then(function(snapshot) {
+    var data = snapshot.val(); //불러온 정보(snapshot)을 javascript로 사용할 수 있게 변경
+
+    name1.innerHTML = name2.innerHTML = data.Name; //상품명
+    img.innerHTML = "<img src= \"" + data.Img + "\">";  //이미지
+    price.innerHTML = "Price : " + data.Price + " won";
+    detail.innerHTML = data.Info;
+
+  });
+}
+getProduct(sessionStorage.Product_no);
--- gift/gift_hashtag.js
+++ gift/gift_hashtag.js
@@ -16,7 +16,6 @@ function getUserName() {
  return firebase.auth().currentUser.displayName;
 }
 
-
 // Returns true if a user is signed-in.
 function isUserSignedIn() {
 return !!firebase.auth().currentUser;
@@ -35,31 +34,31 @@ function authStateObserver(user) {
 
 function getHashtagItem(hashtag){
   var title = document.getElementById("hashtag_title");
-  var img = document.getElementsByClassName("product_img");
-  var name = document.getElementsByClassName("product_name");
-  var price = document.getElementsByClassName("product_detail");
+  title.innerHTML = sessionStorage.Hashtag_name; //hashtag name
+
   var content="";
 
-  title.innerHTML = sessionStorage.Hashtag_name; //hashtag name
   firebase.database().ref().orderByChild('Hashtag_no').equalTo(Number(hashtag)).once('value', function(snapshot){
     var data = snapshot.val();
     data = Object.values(data); //data가 object형으로 나오니까 array로 바꿔주는 것
-
     var content ="";
+
     for (i = 0 ; i < data.length; i++){
       content +=
-      "<tr class='rank_tr' onclick='location.href=\"gift_detail_sender.html\"'> \
-          <td class='rank_no'>" + (i+1) + "</td> \
-          <td class='product_img'><img src='"+ data[i].Img +"'></td> \
+      "<tr class='rank_tr' onclick='sessionStorage.setItem(\"Product_no\", " + data[i].Product_no +
+      "); location.href=\"gift_detail_sender.html\"'> \
+          <td class='rank_no'>" + (i+1) +"</td> \
+          <td class='product_img'><img src='" + data[i].Img + "'></td> \
           <td class='product_info'> \
               <table> \
-                  <tr class='product_name'><td>" +data[i].Name + "</td></tr> \
-                  <tr class='product_detail'><td>" + data[i].Price +" won</td></tr> \
+                  <tr class='product_name'><td>" +data[i].Name +"</td></tr> \
+                  <tr class='product_detail'><td>" +data[i].Price+" won</td></tr> \
               </table> \
           </td> \
       </tr>";
     }
     document.getElementsByClassName("rank")[0].innerHTML = content; //rank div안에 넣기
+    console.log(content);
   });
 }
 
