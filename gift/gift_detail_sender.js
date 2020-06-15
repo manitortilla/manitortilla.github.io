b9bf1cf723c7a5b277599e8e4bba5c50951d180d
Maeg <kimhj5854@naver.com>
2020-06-16 00:55:49 +0900

    QNA board.html, sentBtn 수정

---
+++ gift/gift_detail_sender.js
@@ -0,0 +1,7 @@
+$('.product_buybtn').on('click', function(){
+    alert("The present is sent successfully!");
+});
+
+$('#close').on('click',function(){
+    $('#pop').hide();
+});
---
+++ qnaboard.js
@@ -0,0 +1,36 @@
+// sidebar menu toggle
+$('.sidebarbtn').on('click', function(){
+    //sidebarbtn color update
+    $('.sidebarbtn').removeClass('on');
+    $(this).addClass('on');
+  
+    var idx = $('.sidebarbtn').index(this);
+    $('.detail').hide(); //style="display:none"
+    $('.detail').eq(idx).show();
+    if (idx == 0){ //편지 읽기 상태에서 메뉴 바꿨을 때 메일함이 디폴트로 올 수 있게
+      $('.faqcontent').hide();
+      $('.faqlist').show();
+    }
+});
+
+$('#faqclose').on('click',function(){
+    $('#myModal').hide();
+});
+
+function showfaqcontent(){
+    $('#myModal').show();
+}
+
+function submitqna(){
+    content= document.getElementById("txt").value;
+
+  var t = new Date(+new Date()+(1000*60*60*9));
+
+  if (content != "") {
+    document.getElementById("txt").value = ""; //clear
+    alert("Thank you for submit qna. We will answer as soon as possible.");
+  }
+  else {
+    alert ("You can not submit empty QNA writing.");
+  }
+};
