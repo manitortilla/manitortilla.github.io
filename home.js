1278e08e89d80959b414b254bb4ba1a3c829028c
TK <wrtk007@gmail.com>
2020-06-15 10:47:32 +0900

    1차 수정

--- home.js
+++ home.js
@@ -49,3 +49,19 @@ function authStateObserver(user) {
     location.href="/index.html";
   }
 }
+
+function readmore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+  dots.style.display = "inline";
+  btnText.innerHTML = "Read more";
+  moreText.style.display = "none";
+  } else {
+  dots.style.display = "none";
+  btnText.innerHTML = "Read less";
+  moreText.style.display = "inline";
+  }
+}
