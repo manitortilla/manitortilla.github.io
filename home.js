c845dc76ef48c1a9a3ffaf1a91b7f86204328a22
eun-g-kim <eunji9803@gmail.com>
2020-06-16 17:44:59 +0900

    디데이 가져오기

--- home.js
+++ home.js
@@ -16,9 +16,43 @@ function getUserName() {
  return firebase.auth().currentUser.displayName;
 }
 
+function getUserUid(){ //현재 로그인 한 유저의 uid 불러오기
+  return firebase.auth().currentUser.uid;
+}
 // Returns true if a user is signed-in.
 function isUserSignedIn() {
 return !!firebase.auth().currentUser;
+}
+
+function getDate(){
+  var date = new Date();
+  var year = date.getFullYear();
+  var month = new String(date.getMonth()+1);
+  var day = new String(date.getDate());
+
+  // 한자리수일 경우 0을 채워준다.
+  if(month.length == 1){
+    month = "0" + month;
+  }
+  if(day.length == 1){
+    day = "0" + day;
+  }
+  return year+"-"+month+"-"+day;
+}
+
+
+function updateDday(){
+  var today = getDate();
+  var enddate;
+  var diff;
+  firebase.firestore().collection('gamelist').doc(sessionStorage.gameID).get().then(function(doc){
+    enddate = doc.data().enddate;
+    console.log(today, enddate);
+
+    diff =  Math.floor( (Date.parse(enddate.replace(/-/g,'\/')) - Date.parse(today.replace(/-/g,'\/'))) / 86400000);
+    document.getElementById("Dday").innerHTML = "D-Day : " + diff;
+  });
+
 }
 
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
@@ -27,6 +61,7 @@ function authStateObserver(user) {
    // Get the signed-in user's profile pic and name.
    document.getElementById("userID").innerHTML = getUserName();
    document.getElementById("userID2").innerHTML = getUserName();
+   updateDday();
  } else { // User is signed out!
    location.href="/index.html";
  }
