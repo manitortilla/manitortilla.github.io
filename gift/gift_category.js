b5db63e0bdb9360673f361eec7afdb49983e89f6
eun-g-kim <43198923+eun-g-kim@users.noreply.github.com>
2020-06-17 15:42:15 +0900

    결제 팝업

--- gift/gift_category.js
+++ gift/gift_category.js
@@ -3,6 +3,7 @@ initFirebaseAuth();
 function initFirebaseAuth() {
  firebase.auth().onAuthStateChanged(authStateObserver);
 }
+
 var signOutButtonElement = document.getElementById('logoutbtn');
 function signOut() {
  // Sign out of Firebase.
@@ -15,6 +16,12 @@ function getUserName() {
  return firebase.auth().currentUser.displayName;
 }
 
+
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+return !!firebase.auth().currentUser;
+}
+
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 function authStateObserver(user) {
  if (user) { // User is signed in!
@@ -25,72 +32,46 @@ function authStateObserver(user) {
  }
 }
 
-var menu_cate = document.getElementsByClassName("menu_cate");
-for (i = 0; i < menu_cate.length; i++) {
-  menu_cate[i].addEventListener('click',function(){
-    getItem(this.id);
+
+//pop x button clicked
+$('.product_buybtn').on('click', function(){
+  $('#pop').show();
+});
+$('#popclose').on('click',function(){
+  $('#pop').hide();
+});
+$('#closebtn').on('click',function(){
+  $('#pop').hide();
+});
+$('#submitbtn').on('click', function(){
+  document.getElementsByClassName('popcontent')[0].innerHTML = "Your purchase is successuflly done.";
+  $('.card').hide();
+  $('.buttonwrap').hide();
+});
+
+
+function getProduct(Product_no){
+  document.getElementsByClassName("subt")[0].innerHTML =
+      sessionStorage.Nav_parent + "<span>  > </span><p class='product_name'></p>"; //naviagation bar
+
+  var name1 = document.getElementsByClassName("product_name")[0];
+  var name2 = document.getElementsByClassName("gift_value")[2];
+  var name3 = document.getElementsByClassName("product_name")[1];
+  var img = document.getElementsByClassName("product_img")[0];
+  var price = document.getElementsByClassName("product_price")[0];
+  var detail = document.getElementsByClassName("product_detail")[0];
+
+  return firebase.database().ref(Product_no).once('value').then(function(snapshot) {
+    var data = snapshot.val(); //불러온 정보(snapshot)을 javascript로 사용할 수 있게 변경
+
+    name1.innerHTML = name2.innerHTML = name3.innerHTML = data.Name; //상품명
+    img.innerHTML = "<img src= \"" + data.Img + "\">";  //이미지
+    price.innerHTML = "Price : " + data.Price + " won";
+    detail.innerHTML = data.Info;
+
   });
 }
-
-getItem(0); //always starts from category All
-
-function getItem(categoryNO){
-  var content = "";
-
-  var title = document.getElementById("category_title"); //navigation bar
-  var img = document.getElementsByClassName("product_img");
-  var name = document.getElementsByClassName("product_name");
-  var price = document.getElementsByClassName("product_detail");
-
-  title.innerHTML = document.getElementById(categoryNO).innerHTML; //category name
-  var nav_parent = document.getElementsByClassName("subt")[0].innerHTML;
-  sessionStorage.setItem("Nav_parent", nav_parent);
-
-  if (categoryNO  == 0 ) { //if "all"
-    return firebase.database().ref().once('value').then(function(snapshot) {
-      var data = snapshot.val();
-      for (i = 0 ; i < data.length; i++){
-        content +=
-        "<tr class='rank_tr' \
-         onclick='sessionStorage.setItem(\"Product_no\", " + data[i].Product_no +
-         "); location.href=\"gift_detail_sender.html\"'> \
-            <td class='rank_no'>" + (i+1) + "</td> \
-            <td class='product_img'><img src='"+ data[i].Img +"'></td> \
-            <td class='product_info'> \
-                <table> \
-                    <tr class='product_name'><td>" +data[i].Name + "</td></tr> \
-                    <tr class='product_detail'><td>" + data[i].Price +" won</td></tr> \
-                </table> \
-            </td> \
-        </tr>";
-      }
-      document.getElementsByClassName("rank")[0].innerHTML = content; //rank div안에 넣기
-    });
-  }
-  else { //not "all"
-    return firebase.database().ref().orderByChild('Category_no').equalTo(Number(categoryNO)).once('value', function(snapshot){
-      var data = snapshot.val();
-      data = Object.values(data); //data가 object형으로 나오니까 array로 바꿔주는 것
-
-      for (i = 0 ; i < data.length; i++){
-        content +=
-        "<tr class='rank_tr' \
-          onclick='sessionStorage.setItem(\"Product_no\", " + data[i].Product_no +
-        "); location.href=\"gift_detail_sender.html\"'> \
-            <td class='rank_no'>" + (i+1) + "</td> \
-            <td class='product_img'><img src='"+ data[i].Img +"'></td> \
-            <td class='product_info'> \
-                <table> \
-                    <tr class='product_name'><td>" +data[i].Name + "</td></tr> \
-                    <tr class='product_detail'><td>" + data[i].Price +" won</td></tr> \
-                </table> \
-            </td> \
-        </tr>";
-      }
-      document.getElementsByClassName("rank")[0].innerHTML = content; //rank div안에 넣기
-    });
-  }
-}
+getProduct(sessionStorage.Product_no);
 
 var searchbtn = document.getElementsByClassName("searchbtn")[0];
 var keyword = document.getElementsByClassName("search")[0];
@@ -130,7 +111,7 @@ function gedSearchedItem(key){
     if (found == 0) {
       content = "No such item.";
     }
-    document.getElementsByClassName("rank")[1].innerHTML = content; //rank div안에 넣기
+    document.getElementsByClassName("rank")[0].innerHTML = content; //rank div안에 넣기
     $('.wrapper_search').addClass('on');
     $('.wrapper').hide();
     var nav_bar = document.getElementsByClassName('subt')[0];
