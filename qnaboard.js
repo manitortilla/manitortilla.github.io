f4d3638b9867da119fc88096bcf7df371d9fc30a
eun-g-kim <43198923+eun-g-kim@users.noreply.github.com>
2020-06-21 18:40:53 +0900

    user id 추가

--- qnaboard.js
+++ qnaboard.js
@@ -1,3 +1,29 @@
+initFirebaseAuth();
+// Initiate firebase auth.
+function initFirebaseAuth() {
+ firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+var signOutButtonElement = document.getElementById('logoutbtn');
+function signOut() {
+ // Sign out of Firebase.
+ firebase.auth().signOut();
+}
+signOutButtonElement.addEventListener('click', signOut);
+
+// Returns the signed-in user's display name.
+function getUserName() {
+ return firebase.auth().currentUser.displayName;
+}
+
+function getUserUid(){ //현재 로그인 한 유저의 uid 불러오기
+  return firebase.auth().currentUser.uid;
+}
+// Returns true if a user is signed-in.
+function isUserSignedIn() {
+return !!firebase.auth().currentUser;
+}
+
 // sidebar menu toggle
 $('.sidebarbtn').on('click', function(){
     //sidebarbtn color update
